bddaa1fb179e8e1d5b409b4fa664b7a35ffef58e
kaijabryans <77697770+kaijabryans@users.noreply.github.com>
2021-02-05 12:26:28 -0500

    Assignment 4 Bash

--- MyCount.sh
+++ MyCount.sh
@@ -4,7 +4,8 @@ echo “Wonderfull Loop from 1-20”
 for Count in {1..20}
 
 do if [ $Count -lt 10 ]
-then echo “$Count is a single-digit number”
+then 
+        echo “$Count is a single-digit number”
 
 elif [ $Count -ge 10 ]
 then
